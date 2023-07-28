1b1ec6654a3418ab5b6fc634d4c1c69c3cc94bb1
My <84212641+My-MC@users.noreply.github.com>
2023-07-28 19:49:30 +0900

    Migration to Rollup.js (#50)
    * Migration to Rollup.js
    
    * Fix node condition importing
    
    * smaller

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { swc } from "@rollup/plugin-swc";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import rollupPluginLicense from "rollup-plugin-license";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export default {
+  input: "src/main.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+  },
+  treeshake: "smallest",
+  plugins: [
+    swc(),
+    nodeResolve({
+      extensions: [".mjs", ".js", ".json", ".node", ".ts"],
+      exportConditions: ["node"],
+    }),
+    commonjs(),
+    terser(),
+    rollupPluginLicense({
+      thirdParty: { output: path.join(__dirname, "dist", "licenses.txt") },
+    }),
+  ],
+};
