afc046c72f09e0069edc15ff78c918f335b84f38
My <84212641+My-MC@users.noreply.github.com>
2023-07-28 21:24:53 +0900

    Add type-checked checks (#52)
    * Add type-checked checks and solved problems
    
    * renamed `jest.config.js` to `jest.config.mjs`

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { es6: true, node: true },
+  extends: [
+    "plugin:github/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "eslint:recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    // tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint", "github"],
+  rules: { "github/no-then": "off" },
+};
--- src/check.ts
+++ src/check.ts
@@ -1,8 +1,10 @@
 import { debug } from "@actions/core";
 import { readFileSync } from "fs";
 
-export async function check(packageJsonPath: string, nodeVersionPath: string) {
+export function check(packageJsonPath: string, nodeVersionPath: string) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const packageJsonObj = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   const voltaNodeVersion: string = packageJsonObj.volta.node;
   debug(`volta_node_version: ${voltaNodeVersion}`);
 
